7df17dc2e1a34eadfd05ca5a5d70c7f1615d4c03
Carlos Jimenez <carlosjime512@gmail.com>
2022-06-06 17:41:50 -0500

    primer commit

---
+++ useCounter.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export const useCounter = ( initialState = 1 ) => {
+
+    const [counter, setCounter] = useState(initialState);
+
+    const increment = () => setCounter( counter + 1 );
+
+    const reset = () => setCounter( initialState );
+
+    const decrement = () => {
+        (counter !== 1) && setCounter( counter - 1 )
+    };
+
+    return {
+        counter,
+        increment,
+        reset,
+        decrement
+    };
+}
